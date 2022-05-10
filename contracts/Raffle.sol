5082d5ac25127e746d6f314b72f535761cac0114
Boom <32509869+AnhTuDo1998@users.noreply.github.com>
2022-05-10 10:24:17 +0800

    Update Raffle contract with VRF stuffs

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -3,14 +3,44 @@ pragma solidity ^0.8.4;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "./NFTCollection.sol";
 
-// Function from NFTCollection to be called
-interface INFTCollection {
-    // TODO: Add things ?
-}
+contract Raffle is NFTCollection, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
 
-contract Raffle {
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // Rinkeby coordinator. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords =  2;
+
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    address s_owner;
+
     uint256 public cost = 0.01 ether;
+    uint256 public minNumPlayers = 2;
     uint256 public ballotDate;
 
     // Some choice for storage of lottery player address
@@ -18,30 +48,51 @@ contract Raffle {
     // TODO: Review needed
     address[] lotteryPlayers;
 
-    constructor (address nftContractAddress, uint256 date) {
-        nftCollection = INFTCollection(nftContractAddress);
+    constructor (uint64 subscriptionId, uint256 date) VRFConsumerBaseV2(vrfCoordinator){
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_subscriptionId = subscriptionId;
         ballotDate = date;
     }
 
     function isBallotTime() public view returns (bool) {
-        return (now >= ballotDate);
+        return (block.timestamp >= ballotDate);
     }
 
     // Pay to enter raffle
     function enterRaffle() public payable {
         require(!isBallotTime());
+        require(!paused());
         require(msg.value == cost);
         // TODO: Protection from duplicate entry (or can duplicate?)
-        // TODO: Check for pause as well?
-        lotteryPlayers.push(msg.sender());
+        lotteryPlayers.push(msg.sender);
     }
 
     // Select winner
-    // TODO: Must be call by owner only
-    function drawRaffle() public {
+    function drawRaffle() public onlyRole(DEFAULT_ADMIN_ROLE) {
         require(isBallotTime());
+        require(lotteryPlayers.length >= minNumPlayers);
         // TODO: Integrate VRF here
-        address winner = 0x1234; // hardcoded
-        // TODO: Grant minter role to winner
+        address winner = lotteryPlayers[lotteryPlayers.length - 1]; // hardcoded to last player to enter
+        _grantRole(MINTER_ROLE, winner);
+        // What's next? Only allow 1 specific tokenURI ?
+    }
+
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+        keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+        );
+    }
+    
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
     }
 }
